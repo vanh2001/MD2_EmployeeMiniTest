0e3bb981c8fd6d29c2b91e09050fcef3084942c5
vanh2001 <anhviet1709@gmail.com>
2022-03-07 15:28:30 +0700

    Update doc ghi File

--- Employee_ArrayList/src/model/Employee.java
+++ Employee_ArrayList/src/model/Employee.java
@@ -1,6 +1,8 @@
 package model;
 
-public abstract class Employee {
+import java.io.Serializable;
+
+public abstract class Employee implements Serializable {
     private String employeeId;
     private String fullName;
     private int age;
